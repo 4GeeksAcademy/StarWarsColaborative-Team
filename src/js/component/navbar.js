e0cd50cf599235b04c4460ce37927e6df1200f3f
rocady21 <68871996+rocady21@users.noreply.github.com>
2023-08-18 18:53:38 +0000

    commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+	const navigate = useNavigate()
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 			</Link>
+			<div onClick={()=> navigate("/infoPage")} className="btn btn-primary">
+				InfoPage
+			</div>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+				<div className="dropdown">
+					<button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+						Favoritos 
+					</button>
+					<div className="dropdown-menu"></div>
+				</div>
 			</div>
 		</nav>
 	);
 };
+
