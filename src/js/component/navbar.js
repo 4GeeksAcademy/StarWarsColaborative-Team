fb60526165321c74563fe94d2798402f419a17da
rocady21 <68871996+rocady21@users.noreply.github.com>
2023-08-21 17:58:15 +0000

    mi parte

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
+import {MdDelete} from "react-icons/md"
 
 export const Navbar = () => {
+	const {store,actions} = useContext(Context)
+	const { favoritos } = store
 	const navigate = useNavigate()
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-light mb-3 mx-3">
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 			</Link>
@@ -12,11 +16,19 @@ export const Navbar = () => {
 				InfoPage
 			</div>
 			<div className="ml-auto">
-				<div className="dropdown">
-					<button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-						Favoritos 
+				<div className="dropdown" >
+					<button className="btn btn-primary dropdown-toggle"  type="button" data-bs-toggle="dropdown" aria-expanded="false">
+						Favoritos <span class="badge bg-secondary">{favoritos.length}</span>
 					</button>
-					<div className="dropdown-menu"></div>
+					<div className="dropdown-menu dropdown-menu-end row p-2 " style={{width:"250px"}}>
+						<h3>Favoritos</h3>
+						{
+							favoritos[0]? favoritos.map((item)=>{
+								return <p className="d-flex flex-row justify-content-between align-content-center">{item} <button className="bg-transparent border-0"> <MdDelete/></button></p>
+							}) : <p className="text-dark">No hay Favoritos</p>
+						}
+
+					</div>
 				</div>
 			</div>
 		</nav>
