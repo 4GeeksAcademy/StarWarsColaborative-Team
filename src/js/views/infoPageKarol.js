22465fe7b5262e4420b907cd4dd11809b2de980d
klgs1234 <138398036+klgs1234@users.noreply.github.com>
2023-08-22 18:32:34 +0000

    ultima-version

--- src/js/views/infoPageKarol.js
+++ src/js/views/infoPageKarol.js
@@ -5,6 +5,15 @@ import { Link, useParams } from "react-router-dom";
 const InfoPageKarol =()=> {
     const { store, actions } = useContext(Context)
     const params = useParams()
+    const stylePadre = {
+      width: "100%",
+      overflowX: "scroll",
+      overscrollBehaviorX: "contain",
+      scrollSnapType: "x proximity",
+      maxWidth: "100%",
+      display: "flex",
+      flexDirection: "row",
+    };
 
     useEffect(() => {
         actions.obtenerPeliculaIndividual(params.theid)
@@ -17,14 +26,14 @@ const InfoPageKarol =()=> {
 
 
     return(
-        
-        <div class="card mb-3">
-        <img src={"https://starwars-visualguide.com/assets/img/films/"+(params.theid)+".jpg" } class="card-img-top" alt="..."/>
-        <div class="card-body">
-          <h5 class="card-title">{store.film.title} {store.film.episode_id} </h5>
+        <div class="card mb-3 bg-dark" style={stylePadre}>
+        <img src={"https://starwars-visualguide.com/assets/img/films/"+(params.theid)+".jpg" } class="card-img-top" alt="..." />
+        <div class="card-body text-white">
+         <h1 class="card-title">{store.film.title} {store.film.episode_id} </h1>
           <p class="card-text">{store.film.opening_crawl} </p>
-          
-
+          <p class="card-text"><h5>Director</h5> {store.film.director} </p> 
+          <p class="card-text"><h5>Productor</h5> {store.film.producer} </p> 
+          <p class="card-text"><h5>Fecha de Estreno</h5> {store.film.release_date} </p> 
         </div>
       </div>
     )
