e81bdc8e568c07f04b14934d0a0fb183611c7ce2
Matias Borba <85084252+19Mega@users.noreply.github.com>
2023-09-02 23:49:26 +0000

    update login

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -11,25 +11,26 @@ const getState = ({ getStore, getActions, setStore }) => {
 			planets: [],
 			films: [],
 			starships: [],
-			PageInfo:undefined
+			PageInfo: undefined
 		},
 		actions: {
 
 
 
 
-		// EMPIEZA DESDE >> 21 a 50  FAVORITOS >>> RODRI
-		guardarFavoritos: async (NameFav) => {
-			let existElement = false
-			getStore().favoritos.map((element)=>{
-				if(NameFav === element) {
-					return existElement = true
+			// EMPIEZA DESDE >> 21 a 50  FAVORITOS >>> RODRI
+			guardarFavoritos: async (NameFav) => {
+				let existElement = false
+				getStore().favoritos.map((element) => {
+					if (NameFav === element) {
+						return existElement = true
+					}
+				})
+
+				if (existElement === false) {
+					setStore({ ...getStore(), favoritos: [...getStore().favoritos, NameFav] })
 				}
-			})
 
-			if(existElement === false) {
-				setStore({...getStore(),favoritos:[...getStore().favoritos,NameFav]})
-			}
 
 
 
@@ -46,213 +47,193 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 
 
-
-		},
-		// EMPIEZA DESDE >> 51 a 80  PEOPLE >>> RODRI
-		obtenerPersonas: async () => {
-			console.log("xd")
-			try {
-				const resp = await fetch("https://swapi.dev/api/people")
-				const data = await resp.json()
-				const store = getStore()
-				if(data) {
-					data.results.map((item,index)=> {
-						const uid = index + 1
-						return setStore({...store,peoples:[...store.peoples,{...item,uid:uid}]})
-					})
-				}
-				console.log(store)
-
-			} catch (error) {
-				
-			}
-
-
-
-
-
-
-
-
-
-
-
-		},
-		// EMPIEZA DESDE >> 81 a 110  VEHICLES >>> MATIAS
-
-		getIdfromUrl: itemUrl => { // RECIBE UNA URL ej. https: // swapi.dev / api /vehicles / 4 / y le saca el numero 👹 
-			const urlParts = itemUrl.split('/') // separa la string que llega por los / que tenga y devuelve un array de stringsssss ✨
-			return urlParts[urlParts.length - 2]
-		  },
-	
-		obtenerVehiculos: async () => {
-			try {
-			  const response = await fetch("https://swapi.dev/api/vehicles/")
-			  const data = await response.json()
-	
-			  const vehiculosConId = await Promise.all(
-				data.results.map(async item => { return {...item, id: await getActions().getIdfromUrl(item.url) }
-				})
-			  )
-			  setStore({ vehicles: vehiculosConId });
-			} catch (error) {
-			  console.log(error)
-			}
-		},
-
-
-
-
-
-
-
-
-
-		// EMPIEZA DESDE >> 111 a 140  PLANETS >>> JUAN
-		obtenerPlanetas: async () => {
-			const url = 'https://www.swapi.tech/api/planets';
-			try {
-			  const response = await fetch(url);
-			  const data = await response.json();
-		  
-			  const planetsWithDetails = await Promise.all(
-				data.results.map(async (planet) => {
-				  const detailResponse = await fetch(planet.url);
-				  const detailData = await detailResponse.json();
-				  return {
-					...planet,
-					properties: detailData.result.properties 
-				  };
-				})
-			  );
-			  setStore({ planets: planetsWithDetails });
-			} catch (error) {
-			  console.error('Ocurrió un error:', error);
-			}
-		  },
-		  
-		  
-
-
-
-
-
-		  
-		// EMPIEZA DESDE >> 141 a 170  FILMS >>> KAROL
-		obtenerPeliculas: async () => {
-			const url = 'https://swapi.dev/api/films';
-			try {
-			  const response = await fetch(url);
-			  const data = await response.json();
-				console.log(data)
-				data.results.map((item, index)=>{
-					const id = index+1
-					return setStore({...getStore(),films:[...getStore().films,{...item,id:id}]})
-				})
-			} catch (error) {
-			  console.error('Ocurrió un error:', error);
-			}
-		},
-
-		obtenerPeliculaIndividual: async (id) => {
-			const url = 'https://swapi.dev/api/films/';
-			console.log(id)
-			try {
-			  const response = await fetch(url+id);
-			  const data = await response.json();
-				console.log(data)	
-			  setStore({ film: data });
-			} catch (error) {
-			  console.error('Ocurrió un error:', error);
-			}
-			
-		},
-
-        // EMPIEZA DESDE >> 171 a 200  STARSHIPS >>> YOSELIN
-		obtenerNaves: async () => {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-			
 			},
-		// EMPIEZA DESDE >> 201 a 200  Funcion que filtre datos en base a id >>> Rodrigo
-		TraerInfoEspesifica:async(uid,tipo)=> {
-			console.log(`Estas buscando la api aca https://swapi.dev/api/${tipo}/${uid}`)
-			try {
+			// EMPIEZA DESDE >> 51 a 80  PEOPLE >>> RODRI
+			obtenerPersonas: async () => {
+				console.log("xd")
+				try {
+					const resp = await fetch("https://swapi.dev/api/people")
+					const data = await resp.json()
+					const store = getStore()
+					if (data) {
+						data.results.map((item, index) => {
+							const uid = index + 1
+							return setStore({ ...store, peoples: [...store.peoples, { ...item, uid: uid }] })
+						})
+					}
+					console.log(store)
+
+				} catch (error) {
+
+				}
+
+
+
+
+
+
+
+
+
+
+
+			},
+			// EMPIEZA DESDE >> 81 a 110  VEHICLES >>> MATIAS
+
+			getIdfromUrl: itemUrl => { // RECIBE UNA URL ej. https: // swapi.dev / api /vehicles / 4 / y le saca el numero 👹 
+				const urlParts = itemUrl.split('/') // separa la string que llega por los / que tenga y devuelve un array de stringsssss ✨
+				return urlParts[urlParts.length - 2]
+			},
+
+			obtenerVehiculos: async () => {
+				try {
+					const response = await fetch("https://swapi.dev/api/vehicles/")
+					const data = await response.json()
+		
+					const vehiculosConId = await Promise.all(
+						data.results.map(async item => {
+							return { ...item, id: await getActions().getIdfromUrl(item.url) }
+						})
+					)
+					setStore({ vehicles: vehiculosConId });
+				} catch (error) {
+					console.log(error)
+				}
+			},
+
+
+
+
+
+
+
+
+
+			// EMPIEZA DESDE >> 111 a 140  PLANETS >>> JUAN
+			obtenerPlanetas: async () => {
+				const url = 'https://www.swapi.tech/api/planets';
+				try {
+					const response = await fetch(url);
+					const data = await response.json();
+
+					const planetsWithDetails = await Promise.all(
+						data.results.map(async (planet) => {
+							const detailResponse = await fetch(planet.url);
+							const detailData = await detailResponse.json();
+							return {
+								...planet,
+								properties: detailData.result.properties
+							};
+						})
+					);
+					setStore({ planets: planetsWithDetails });
+				} catch (error) {
+					console.error('Ocurrió un error:', error);
+				}
+			},
+
+
+
+
+
+
+
+
+			// EMPIEZA DESDE >> 141 a 170  FILMS >>> KAROL
+			obtenerPeliculas: async () => {
+				const url = 'https://swapi.dev/api/films';
+				try {
+					const response = await fetch(url);
+					const data = await response.json();
+					console.log(data)
+					data.results.map((item, index) => {
+						const id = index + 1
+						return setStore({ ...getStore(), films: [...getStore().films, { ...item, id: id }] })
+					})
+				} catch (error) {
+					console.error('Ocurrió un error:', error);
+				}
+			},
+
+			obtenerPeliculaIndividual: async (id) => {
+				const url = 'https://swapi.dev/api/films/';
+				console.log(id)
+				try {
+					const response = await fetch(url + id);
+					const data = await response.json();
+					console.log(data)
+					setStore({ film: data });
+				} catch (error) {
+					console.error('Ocurrió un error:', error);
+				}
+
+			},
+
+			// EMPIEZA DESDE >> 171 a 200  STARSHIPS >>> YOSELIN
+			obtenerNaves: async () => {
+
+					
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			},
+			// EMPIEZA DESDE >> 201 a 200  Funcion que filtre datos en base a id >>> Rodrigo
+			TraerInfoEspesifica: async (uid, tipo) => {
 				console.log(`Estas buscando la api aca https://swapi.dev/api/${tipo}/${uid}`)
-				const resp = await fetch(`https://swapi.dev/api/${tipo}/${uid}`)
-				const data = await resp.json()
-				console.log(data)
-				setStore({...getStore(),PageInfo:data})
-				
-			} catch (error) {
-				console.log(error)
-			}
-		},
+				try {
+					console.log(`Estas buscando la api aca https://swapi.dev/api/${tipo}/${uid}`)
+					const resp = await fetch(`https://swapi.dev/api/${tipo}/${uid}`)
+					const data = await resp.json()
+					console.log(data)
+					setStore({ ...getStore(), PageInfo: data })
 
-		// FAVORITOS
-		BorrarFavorito:(elements)=> {
-			const Favs = getStore().favoritos
+				} catch (error) {
+					console.log(error)
+				}
+			},
 
-			const newArr = Favs.filter((item)=> {
-				return item!== elements
-			})
+			// FAVORITOS
+			BorrarFavorito: (elements) => {
+				const Favs = getStore().favoritos
 
-			setStore({...getStore(),favoritos:newArr})
-		}
-
-		},
-
-		// LOGIN
-		userLogin: async () => {
-			try {
-				
-			  const response = await fetch("https://swapi.dev/api/vehicles/")
-			  const data = await response.json()
-	
-			  const vehiculosConId = await Promise.all(
-				data.results.map(async item => { return {...item, id: await getActions().getIdfromUrl(item.url) }
+				const newArr = Favs.filter((item) => {
+					return item !== elements
 				})
-			  )
-			  setStore({ vehicles: vehiculosConId });
-			} catch (error) {
-			  console.log(error)
+
+				setStore({ ...getStore(), favoritos: newArr })
+			},
+
+			loginUser: (email,password) =>{
+				console.log(" FLUX USER LOGIN: ", email, " >>>> ", password )
 			}
+
+
+
+
+
 		},
-
-
-
-
-
-
-
-
-
-
-
 
 	}
 }
